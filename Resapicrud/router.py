5f6c5a10b7deeb6548b0127f1ad7cbad8c93dfd5
M4hf0d <ma.sayah@esi-sba.dz>
2022-04-24 18:39:23 +0200

    initial commit

---
+++ Resapicrud/router.py
@@ -0,0 +1,5 @@
+from employeeapi.viewsets import EmployeeViewset
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('employee',EmployeeViewset)
---
+++ employeeapi/urls.py
@@ -0,0 +1,7 @@
+from zipfile import Path
+from django.urls import path,include
+from . import views
+
+
+urlpatterns = []
+
